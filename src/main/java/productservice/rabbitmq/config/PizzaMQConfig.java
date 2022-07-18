bd37f0406084f75604e2082e1022f4780db3c1b1
EddeCCC <eduard.schander@gmx.de>
2022-07-18 11:40:06 +0200

    add ingredientBindings

--- src/main/java/productservice/rabbitmq/config/PizzaMQConfig.java
+++ src/main/java/productservice/rabbitmq/config/PizzaMQConfig.java
@@ -1,22 +1,15 @@
 package productservice.rabbitmq.config;
 
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MQConfig {
+public class PizzaMQConfig {
 
     @Bean
     public TopicExchange pizzaExchange() { return new TopicExchange(Constant.PIZZA_EXCHANGE); }
-
-    @Bean
-    public TopicExchange ingredientExchange() { return new TopicExchange(Constant.INGREDIENT_EXCHANGE); }
 
     @Bean
     public Queue getAllPizzaQueue() { return new Queue(Constant.GETALL_PIZZA_QUEUE, false); }
@@ -35,7 +28,7 @@ public class MQConfig {
 
     @Bean
     public Binding getPizzaByIDBinding(@Qualifier("getPizzaQueue") Queue queue,
-                                   @Qualifier("pizzaExchange") TopicExchange exchange) {
+                                       @Qualifier("pizzaExchange") TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(Constant.GET_KEY);
     }
 
@@ -43,15 +36,5 @@ public class MQConfig {
     public Binding createPizzaBinding(@Qualifier("createPizzaQueue") Queue queue,
                                       @Qualifier("pizzaExchange") TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(Constant.CREATE_KEY);
-    }
-
-    @Bean
-    public MessageConverter messageConverter() { return new Jackson2JsonMessageConverter(); }
-
-    @Bean
-    public AmqpTemplate template(ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(messageConverter());
-        return template;
     }
 }
---
+++ src/main/java/productservice/rabbitmq/receiver/IngredientReceiver.java
@@ -0,0 +1,48 @@
+package productservice.rabbitmq.receiver;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import productservice.api.controller.IngredientController;
+import productservice.api.dto.IngredientDTO;
+import productservice.api.service.IngredientDTOMapper;
+import productservice.rabbitmq.config.Constant;
+
+import java.util.List;
+
+@Component
+@Transactional
+public class IngredientReceiver {
+
+    @Autowired
+    IngredientController ingredientController;
+
+    @RabbitListener(queues = Constant.GETALL_INGREDIENT_QUEUE)
+    public List<IngredientDTO> getIngredients() {
+        ResponseEntity<List<IngredientDTO>> entity;
+        try {
+            entity = ingredientController.getIngredients();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        List<IngredientDTO> allIngredients = entity.getBody();
+        return allIngredients;
+    }
+
+    @RabbitListener(queues = Constant.GET_INGREDIENT_QUEUE)
+    public IngredientDTO getIngredientByID(@Payload Long id) {
+        ResponseEntity<IngredientDTO> entity;
+        try {
+            entity = ingredientController.getIngredientById(id);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        IngredientDTO ingredientDTO = entity.getBody();
+        return ingredientDTO;
+    }
+}
